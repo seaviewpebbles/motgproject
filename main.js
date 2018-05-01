173afc433a380e08f10a0b3e7294ee1f43eeacb1
Yardman <yardman@Yardmans-MacBook-Pro.local>
2018-04-30 23:07:22 -0400

    Fixed page posting data when validation failed

--- main.js
+++ main.js
@@ -12,47 +12,109 @@
 // reference messages collection
 var messagesRef = firebase.database().ref('messages');
 
+$(document).ready(function () {
+  //listen for fork submit
+  $('#onlineqoute').on('submit', submitForm);
 
-//listen for fork submit
-document.getElementById('onlineqoute').addEventListener('submit', submitForm);
-function submitForm(e){
-e.preventDefault();
+  $('#myImg').on({
+    mouseover: function () {
+      $(this).css({
+        'height': 250, 'width': 250
 
-//get values
-var name = getInputVal('name');
-var email = getInputVal('email');
-var valid_age = $('input[name=age]:checked').val();
-var additionalinfo = getInputVal('additionalinfo');
-debugger;
-//save message
-saveMessage(name, email, valid_age, additionalinfo);
+      });
+    },
 
-//show alert
-document.querySelector('.alert').style.display = 'block';
+    mouseout: function () {
+      $(this).css({
+        'height': 153, 'width': 135
 
-//hide alert after 3 seconds
-setTimeout(function(){
+      });
+    }
+  });
+});
+
+function submitForm(e) {
+  e.preventDefault();
+  debugger;
+  // Validation
+  if (!is_checked()) {
+    clearForm();
+    return;
+  }
+
+  //get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var valid_age = $('input[name=age]:checked').val();
+  var additionalinfo = getInputVal('additionalinfo');
+  //save message
+  saveMessage(name, email, valid_age, additionalinfo);
+
+  //show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  //hide alert after 3 seconds
+  setTimeout(function () {
     document.querySelector('.alert').style.display = 'none';
-},3000);
+  }, 3000);
 
-//clear form
-document.getElementById('onlineqoute').reset();
+  //clear form
+  clearForm();
 }
 
 //function to get form values
-function getInputVal(id){
-    return document.getElementById(id).value;
+function getInputVal(id) {
+  return document.getElementById(id).value;
 }
 
- //save message to firebase
+//save message to firebase
+function saveMessage(name, email, valid_age, additionalinfo) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    valid_age: valid_age,
+    additionalinfo: additionalinfo
 
- function saveMessage(name, email, valid_age, additionalinfo ){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-name:name,
-email:email,
-valid_age:valid_age,
-additionalinfo:additionalinfo
+  });
+}
 
-    });
+function MM_validateForm() { //v4.0
+  if (document.getElementById) {
+    var i, p, q, nm, test, num, min, max, errors = '', args = MM_validateForm.arguments;
+    for (i = 0; i < (args.length - 2); i += 3) {
+      test = args[i + 2]; val = document.getElementById(args[i]);
+      if (val) {
+        nm = val.name; if ((val = val.value) != "") {
+          if (test.indexOf('isEmail') != -1) {
+            p = val.indexOf('@');
+            if (p < 1 || p == (val.length - 1)) errors += '- ' + nm + ' must contain an e-mail address.\n';
+          } else if (test != 'R') {
+            num = parseFloat(val);
+            if (isNaN(val)) errors += '- ' + nm + ' must contain a number.\n';
+            if (test.indexOf('inRange') != -1) {
+              p = test.indexOf(':');
+              min = test.substring(8, p); max = test.substring(p + 1);
+              if (num < min || max < num) errors += '- ' + nm + ' must contain a number between ' + min + ' and ' + max + '.\n';
+            }
+          }
+        } else if (test.charAt(0) == 'R') errors += '- ' + nm + ' is required.\n';
+      }
+    } if (errors) alert('The following error(s) occurred:\n' + errors);
+    document.MM_returnValue = (errors == '');
   }
+}
+
+function is_checked() {
+  var age_valid = $('input[name=age]:checked').val();
+  if (age_valid == 'no') {
+    alert('You are under age to use this site');
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function clearForm() {
+  document.getElementById('onlineqoute').reset();
+}
